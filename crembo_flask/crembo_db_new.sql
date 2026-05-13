41154a2a1f905af03c194c26a8033d38722a0e2b
AtanasiusSuryaGunadharma <220711667@students.uajy.ac.id>
2026-05-13 20:02:45 +0700

    Fix page manajemen anggota, delete feature import excel and adding number

--- crembo_flask/crembo_db_new.sql
+++ crembo_flask/crembo_db_new.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Bulan Mei 2026 pada 12.52
+-- Waktu pembuatan: 13 Bulan Mei 2026 pada 14.35
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
